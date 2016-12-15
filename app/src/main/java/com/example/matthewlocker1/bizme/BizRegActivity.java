66cf01652838b0b92d9062f4f2f7cb1163f30f55
CodeLockr <matthewlocker1@Matthews-MacBook-Air.local>
2016-12-15 17:30:39 -0500

    Initial Project Commit

---
+++ app/src/main/java/com/example/matthewlocker1/bizme/BizRegActivity.java
@@ -0,0 +1,13 @@
+package com.example.matthewlocker1.bizme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BizRegActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biz_reg);
+    }
+}
---
+++ app/src/main/java/com/example/matthewlocker1/bizme/FeedActivity.java
@@ -0,0 +1,13 @@
+package com.example.matthewlocker1.bizme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FeedActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
