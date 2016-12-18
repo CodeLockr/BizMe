c102c489b42122605ab2e8137e607132312c4909
CodeLockr <mlocker@umich.edu>
2016-12-18 17:15:14 -0500

    Final version 5 PM

--- app/src/main/java/com/example/matthewlocker1/bizme/RegistrationActivity.java
+++ app/src/main/java/com/example/matthewlocker1/bizme/RegistrationActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -116,7 +118,59 @@ public class RegistrationActivity extends Activity implements View.OnClickListen
                     }
                 });
 
-    }}
+    }
+
+    //added menus
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intentMain = new Intent(RegistrationActivity.this, MainActivity.class);
+        Intent intentFeed = new Intent(RegistrationActivity.this, FeedActivity.class);
+        Intent intentMap = new Intent(RegistrationActivity.this, MapsActivity.class);
+        Intent intentProfile = new Intent(RegistrationActivity.this, ProfileActivity.class);
+
+        if (mAuth.getCurrentUser() != null) {
+            if (item.getItemId() == R.id.menuLogout) {
+                mAuth.signOut();
+                startActivity(intentMain);
+            } else if (item.getItemId() == R.id.menuFeed) {
+                startActivity(intentFeed);
+            } else if (item.getItemId () == R.id.menuMap){
+                startActivity(intentMap);
+            } else if (item.getItemId() == R.id.menuProfile) {
+                startActivity(intentProfile);
+            }
+
+        } else {
+            Toast.makeText(this, "Nobody Logged In", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+}
 
 
 
